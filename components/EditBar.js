70d35cd1c909bfc865ec3b9729c186302c6d69ae
luigi <luigilamastra@gmail.com>
2022-03-18 09:22:15 +0100

    changed icons

--- components/EditBar.js
+++ components/EditBar.js
@@ -2,43 +2,48 @@ import React from 'react';
 import EditButton from './EditButton';
 import useStore from '../store';
 import { FaAlignCenter, FaAlignLeft, FaBold,  FaItalic, FaUndo, FaParagraph, FaHeading} from "react-icons/fa";
+import {BsTextCenter, BsTextLeft, BsTypeBold, BsTypeItalic, BsArrowCounterclockwise, BsTypeH1, BsTextParagraph} from 'react-icons/bs'
+import {IconContext} from 'react-icons'
 function EditBar() {
   const commandState = useStore((state) => state.commandState);
 
   return (
+
+
+    <IconContext.Provider value={{size: '1.1rem'}}>
     <div className='editbar'>
       <EditButton cmd='bold' name='bold' > 
-      <FaBold></FaBold>
+      <BsTypeBold />
       </EditButton>
       <EditButton cmd='italic' name='italic' >
-        <FaItalic/>
+        <BsTypeItalic/>
          </EditButton>
 
       {commandState.heading ? (
         <EditButton cmd='formatBlock' arg='div' name='text' >
 
-<FaParagraph/>
+<BsTextParagraph/>
 
         </EditButton>
       ) : (
         <EditButton cmd='formatBlock' arg='h1' name='heading' >
-          <FaHeading/>
+          <BsTypeH1/>
           
            </EditButton>
       )}
 
       {commandState.center ? (
         <EditButton cmd='justifyLeft' name='left'  > 
-        <FaAlignLeft />
+        <BsTextLeft />
         </EditButton>
       ) : (
         <EditButton cmd='justifyCenter' name='center' > 
         
-        <FaAlignCenter/>
+        <BsTextCenter/>
         </EditButton>
       )}
  <EditButton cmd='undo' name='undo' >
-   <FaUndo/>
+   <BsArrowCounterclockwise/>
    
     </EditButton>
 
@@ -52,6 +57,12 @@ function EditBar() {
         `}
       </style>
     </div>
+    
+    
+    
+    
+    
+    </IconContext.Provider>
   );
 }
 
